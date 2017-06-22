366cdbd39c872abc706b690c6a8af1367f393084
Anthony Cole <anthony@swiftnav.com>
2017-06-22 13:20:44 -0700

    Add initial c RTCM decoder support

---
+++ c/include/common.h
@@ -0,0 +1,28 @@
+//
+// Created by Anthony Cole on 6/26/17.
+//
+
+#ifndef LIBRTCM_COMMON_H
+#define LIBRTCM_COMMON_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/** Signed 8-bit integer. */
+typedef int8_t s8;
+/** Signed 16-bit integer. */
+typedef int16_t s16;
+/** Signed 32-bit integer. */
+typedef int32_t s32;
+/** Signed 64-bit integer. */
+typedef int64_t s64;
+/** Unsigned 8-bit integer. */
+typedef uint8_t u8;
+/** Unsigned 16-bit integer. */
+typedef uint16_t u16;
+/** Unsigned 32-bit integer. */
+typedef uint32_t u32;
+/** Unsigned 64-bit integer. */
+typedef uint64_t u64;
+
+#endif //LIBRTCM_COMMON_H
