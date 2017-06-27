599b1b76c8b0eb3b8599ccc2641e55644bc65cbc
Anthony Cole <anthony@swiftnav.com>
2017-06-26 22:32:55 -0700

    Fix copyright

--- c/include/common.h
+++ c/include/common.h
@@ -1,6 +1,14 @@
-//
-// Created by Anthony Cole on 6/26/17.
-//
+/*
+ * Copyright (C) 2017 Swift Navigation Inc.
+ * Contact: Swift Navigation <dev@swiftnav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
 #ifndef LIBRTCM_COMMON_H
 #define LIBRTCM_COMMON_H
--- c/include/rtcm3_messages.h
+++ c/include/rtcm3_messages.h
@@ -1,6 +1,14 @@
-//
-// Created by pgrgich on 3/14/17.
-//
+/*
+ * Copyright (C) 2017 Swift Navigation Inc.
+ * Contact: Swift Navigation <dev@swiftnav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
 #ifndef PIKSI_BUILDROOT_RTCM3_MESSAGES_H_H
 #define PIKSI_BUILDROOT_RTCM3_MESSAGES_H_H
