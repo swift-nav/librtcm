2cd7c3cab74703a809472f01351b0e4f24ed2b7d
Mark Fine <mark.fine@gmail.com>
2018-04-06 17:45:14 -0700

    Add 1029 support.

--- haskell/src/Data/RTCM3/System.hs
+++ haskell/src/Data/RTCM3/System.hs
@@ -24,6 +24,7 @@ import           Data.Binary.Bits
 import qualified Data.Binary.Bits.Get as B
 import qualified Data.Binary.Bits.Put as B
 import           Data.RTCM3.TH
+import qualified Data.ByteString as BS
 
 {-# ANN module ("HLint: ignore Use camelCase"::String) #-}
 
@@ -93,6 +94,49 @@ instance BinaryBit Message where
     B.putBool        _message_synchronous
     B.putWord16be 16 _message_interval
 
+-- | TextMessage
+--
+-- Informational text message.
+data TextMessage = TextMessage
+  { _textMessage_num         :: Word16
+    -- ^ Message number.
+  , _textMessage_station     :: Word16
+    -- ^ Reference station id.
+  , _textMessage_mjd         :: Word16
+    -- ^ Modified Julian Day Number.
+  , _textMessage_seconds     :: Word32
+    -- ^ Seconds of Day.
+  , _textMessage_characters  :: Word8
+    -- ^ Number of characters.
+  , _textMessage_n           :: Word8
+    -- ^ Number of UTF-8 code units.
+  , _textMessage_text        :: Text
+    -- ^ UTF-8 code units.
+  } deriving ( Show, Read, Eq )
+
+$(makeLenses ''TextMessage)
+$(deriveJSON defaultOptions {fieldLabelModifier = fromMaybe "_textMessage_" . stripPrefix "_textMessage_"} ''TextMessage)
+
+instance BinaryBit TextMessage where
+  getBits _n = do
+    _textMessage_num        <- B.getWord16be 12
+    _textMessage_station    <- B.getWord16be 12
+    _textMessage_mjd        <- B.getWord16be 16
+    _textMessage_seconds    <- B.getWord32be 17
+    _textMessage_characters <- B.getWord8 7
+    _textMessage_n          <- B.getWord8 8
+    _textMessage_text       <- decodeUtf8 . BS.pack <$> replicateM (fromIntegral _textMessage_n) (B.getWord8 8)
+    pure TextMessage {..}
+
+  putBits _n TextMessage {..} = do
+    B.putWord16be 12 _textMessage_num
+    B.putWord16be 12 _textMessage_station
+    B.putWord16be 16 _textMessage_mjd
+    B.putWord32be 17 _textMessage_seconds
+    B.putWord8 7     _textMessage_characters
+    B.putWord8 8     _textMessage_n
+    forM_ (BS.unpack $ encodeUtf8 _textMessage_text) $ B.putWord8 8
+
 msg1013 :: Word16
 msg1013 = 1013
 
@@ -120,3 +164,28 @@ instance Binary Msg1013 where
     forM_ _msg1013_messages $ putBits 0
 
 $(deriveRTCM3 ''Msg1013)
+
+msg1029 :: Word16
+msg1029 = 1029
+
+-- | Msg1029.
+--
+-- RTCMv3 message 1029.
+newtype Msg1029 = Msg1029
+  { _msg1029_textMessage :: TextMessage
+    -- ^ Text Messages.
+  } deriving ( Show, Read, Eq )
+
+$(makeLenses ''Msg1029)
+$(deriveJSON defaultOptions {fieldLabelModifier = fromMaybe "_msg1029_" . stripPrefix "_msg1029_"} ''Msg1029)
+
+instance Binary Msg1029 where
+  get = B.runBitGet $ do
+    _msg1029_textMessage <- getBits 0
+    pure Msg1029 {..}
+
+  put Msg1029 {..} = B.runBitPut $
+    putBits 0 _msg1029_textMessage
+
+$(deriveRTCM3 ''Msg1029)
+
